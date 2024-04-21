17ea64f807f72784ea259eff044c177fab08786c
alcode111 <al.code.111@gmail.com>
2024-04-21 14:30:30 +0200

    Initial Commit

---
+++ SwiftUIPracticeAPITheOffice/SwiftUIPracticeAPITheOfficeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeAPITheOfficeApp.swift
+//  SwiftUIPracticeAPITheOffice
+//
+//  Created by Ismaïl LP on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeAPITheOfficeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
