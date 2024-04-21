37a551d3c2ac339407590b5116d3e088abae6acd
alcode111 <al.code.111@gmail.com>
2024-04-21 16:24:54 +0200

    Replace image, continue building QuoteView

--- SwiftUIPracticeAPITheOffice/QuoteView.swift
+++ SwiftUIPracticeAPITheOffice/QuoteView.swift
@@ -13,8 +13,80 @@ struct QuoteView: View {
     
     var body: some View {
         GeometryReader { geo in
-            
+            ZStack(alignment: .top) {
+                Image("the-office")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(
+                        width: geo.size.width * 2.7,
+                        height: geo.size.height * 1.34)
+                
+                
+                VStack {
+                    VStack {
+                        Spacer(minLength: 140)
+
+                        switch vm.status {
+                        case .success(let data):
+                            ZStack(alignment: .bottom) {
+                                AsyncImage(url: data.characterAvatarUrl) { image in
+                                    image
+                                        .resizable()
+                                        .scaledToFill()
+                                } placeholder: {
+                                    ProgressView()
+                                }
+                                
+//                                Text(data.character) {
+//                                    .foregroundStyle(.white)
+//                                    .padding(10)
+//                                    .frame(maxWidth: .infinity)
+//                                    .background(.ultraThinMaterial)
+                            }
+                            .frame(
+                                width: geo.size.width / 1.1,
+                                height: geo.size.height / 1.8)
+                            .clipShape(RoundedRectangle(cornerRadius: 80))
+                            
+                            Text(data.quote)
+                                .multilineTextAlignment(.center)
+                                .foregroundStyle(.white)
+                                .padding()
+                                .background(.black.opacity(0.5))
+                                .cornerRadius(25)
+                                .padding(.horizontal)
+                            
+                        case .fetching:
+                            ProgressView()
+                            
+                        default:
+                            EmptyView()
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        Task {
+                            await vm.getData()
+                        }
+                    } label: {
+                        Text("Get Random Quote")
+                            .font(.title)
+                            .foregroundStyle(.white)
+                            .padding()
+                            .background(.black)
+                            .clipShape(.rect(cornerRadius: 8))
+                    }
+                    
+                    Spacer(minLength: 180)
+
+                }
+                .frame(width: geo.size.width)
+            }
+            .frame(width: geo.size.width, height: geo.size.height)
         }
+        .ignoresSafeArea()
     }
 }
 
