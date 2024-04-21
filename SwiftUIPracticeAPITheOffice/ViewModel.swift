d4a824d697241482f83350e19cf91b4c04efcb23
alcode111 <al.code.111@gmail.com>
2024-04-21 15:30:01 +0200

    Create ViewModel

---
+++ SwiftUIPracticeAPITheOffice/ViewModel.swift
@@ -0,0 +1,36 @@
+//
+//  ViewModel.swift
+//  SwiftUIPracticeAPITheOffice
+//
+//  Created by Ismaïl LP on 21/04/2024.
+//
+
+import Foundation
+
+@MainActor
+class ViewModel: ObservableObject {
+    enum Status {
+        case notStarted
+        case fetching
+        case success(data: (Quote))
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var status: Status = .notStarted
+    
+    private let controller: FetchController
+    
+    init(controller: FetchController) {
+        self.controller = controller
+    }
+    
+    func getData() async {
+        status = .fetching
+        
+        do {
+            let quote = try await controller.fetchQuote()
+        } catch {
+            status = .failed(error: error)
+        }
+    }
+}
