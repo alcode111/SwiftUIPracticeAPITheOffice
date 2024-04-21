1da4682a08b4e6c6d1025f126aad74c2d11e27f1
alcode111 <al.code.111@gmail.com>
2024-04-21 15:25:37 +0200

    Create FetchController

---
+++ SwiftUIPracticeAPITheOffice/FetchController.swift
@@ -0,0 +1,35 @@
+//
+//  FetchController.swift
+//  SwiftUIPracticeAPITheOffice
+//
+//  Created by Ismaïl LP on 21/04/2024.
+//
+
+import Foundation
+
+struct FetchController {
+    enum NetworkError: Error {
+        case badURL, badResponse
+    }
+    
+    private let baseURL = URL(string: "https://akashrajpurohit.github.io/the-office-api")!
+    
+    func fetchQuote() async throws -> Quote {
+        let quoteURL = baseURL.appending(path: "quotes/random")
+        var quoteComponents = URLComponents(url: quoteURL, resolvingAgainstBaseURL: true)
+        
+        guard let fetchURL = quoteComponents?.url else {
+            throw NetworkError.badURL
+        }
+        
+        let (data, response) = try await URLSession.shared.data(from: fetchURL)
+        
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw NetworkError.badResponse
+        }
+        
+        let quote = try JSONDecoder().decode(Quote.self, from: data)
+        
+        return quote
+    }
+}
